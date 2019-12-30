453e3048d9cfb134649e44e1a106f752761212b9
Engin Kayali <engin.kayali@beyn.com.tr>
2019-12-30 19:08:25 +0300

    ana görünüm ayarlandı

--- App.js
+++ App.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView,  Text, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start your app hhh hhh! rgrg</Text>
+      <View style={styles.navbar}>
+         <ScrollView horizontal={true}>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navbarItem}>
+                <Text>Test</Text>
+            </TouchableOpacity>
+         </ScrollView>
+       </View>
+       <View style={styles.content}>
+            <ScrollView>
+                  <TouchableOpacity style={styles.contentItem}>
+                      <Text> Touch Here </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.contentItem}>
+                      <Text> Touch Here </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.contentItem}>
+                      <Text> Touch Here </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.contentItem}>
+                      <Text> Touch Here </Text>
+                  </TouchableOpacity>
+            </ScrollView>
+        </View>
+        <View style={styles.promo}>
+              <Text> Promo side </Text>
+         </View>
     </View>
   );
 }
@@ -12,8 +64,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ddd',
+    marginTop: 24
   },
+  navbar: {
+      flex: 1,
+      backgroundColor: '#fff',
+      flexDirection: 'column',
+      paddingTop: 3,
+      paddingLeft:1,
+      paddingRight:1
+  },
+  navbarItem: {
+    width:100,
+    height:78,
+    marginRight: 1,
+    marginLeft:1,
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: '#dcc',
+  },
+  content: {
+    flex:6,
+    backgroundColor: '#fff',
+    paddingLeft:2,
+    paddingRight:2
+  },
+  contentItem: {
+    flex: 1,
+    height:100,
+    backgroundColor: '#bbdefb',
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: '#dcc',
+    marginBottom:2,
+  },
+  promo: {
+    flex:1,
+    backgroundColor: '#d9e3f0'
+  }
 });
